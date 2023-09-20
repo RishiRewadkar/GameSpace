7d5171139a72cdc3f0bc035058479f84acb09e24
Arushi Goyal <arushi@Arushis-MacBook-Pro.local>
2023-09-19 19:02:33 -0700

    Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0f;
+    public GameManager gameManager;
+
+    void Update()
+    {
+        // Check if the game is not over
+        if (!gameManager.IsGameOver())
+        {
+            // Update the timer
+            timer += Time.deltaTime;
+        }
+    }
+
+    void OnGUI()
+    {
+        // Convert timer to minutes and seconds
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
+
+        Rect labelRect = new Rect(Screen.width - 120, 10, 100, 40);
+
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.fontSize = 24; // Set the font size to 24
+
+        // Display the timer with the updated style and position
+        GUI.Label(labelRect, string.Format("{0:00}:{1:00}", minutes, seconds), style);
+
+    }
+
+    // Rest of your code...
+}
+
